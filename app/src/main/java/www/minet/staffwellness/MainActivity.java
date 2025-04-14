0597dfd0039b1f929976abec3235cbffcf3109ac
slimripah <slimripah@gmail.com>
2025-04-14 11:15:51 +0300

    splash screen

--- app/src/main/java/www/minet/staffwellness/MainActivity.java
+++ app/src/main/java/www/minet/staffwellness/MainActivity.java
@@ -1,6 +1,13 @@
 package www.minet.staffwellness;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +16,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Variables
+    ImageView backgroundImage;
+    TextView poweredByLine;
+    RelativeLayout relativeLayout;
+
+    //Animations
+    Animation bottomAnim, sideAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +36,41 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //Hooks
+        backgroundImage = findViewById(R.id.background_image);
+        poweredByLine = findViewById(R.id.powered_by_line);
+        relativeLayout = findViewById(R.id.relay);
+
+        //Animations
+        sideAnim = AnimationUtils.loadAnimation(this, R.anim.side_anim);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_anim);
+
+        //Set Animation On Elements
+        backgroundImage.setAnimation(sideAnim);
+        poweredByLine.setAnimation(bottomAnim);
+
+        //First Animation
+        backgroundImage.animate().alpha(0f).setDuration(4000);
+        poweredByLine.animate().alpha(0f).setDuration(4000);
+
+        //Second Animation
+        relativeLayout.animate().alpha(1f).setDuration(4000);
+
+        //Function
+        int SPLASH_TIMER = 8000;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(getApplicationContext(), Dashboard.class);
+                startActivity(intent);
+                finish();
+
+            }
+
+        }, SPLASH_TIMER);
 
     }
 
