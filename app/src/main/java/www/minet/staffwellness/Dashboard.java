5c78f54b6d68619e60adcca6199889f2e74bf6e8
slimripah <slimripah@gmail.com>
2025-04-16 13:35:03 +0300

    dashboard progress

--- app/src/main/java/www/minet/staffwellness/Dashboard.java
+++ app/src/main/java/www/minet/staffwellness/Dashboard.java
@@ -1,6 +1,8 @@
 package www.minet.staffwellness;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +24,25 @@ public class Dashboard extends AppCompatActivity {
             return insets;
         });
 
+    }
+
+    private void animateRings() {
+        ImageView ringBlue = findViewById(R.id.ringBlue);
+        ImageView ringGreen = findViewById(R.id.ringGreen);
+        ImageView ringRed = findViewById(R.id.ringRed);
+
+        ObjectAnimator blueAnim = ObjectAnimator.ofInt(ringBlue, "level", 0, 10000);
+        blueAnim.setDuration(1500);
+
+        ObjectAnimator greenAnim = ObjectAnimator.ofInt(ringGreen, "level", 0, 8000);
+        greenAnim.setDuration(1500);
+
+        ObjectAnimator redAnim = ObjectAnimator.ofInt(ringRed, "level", 0, 6000);
+        redAnim.setDuration(1500);
+
+        blueAnim.start();
+        greenAnim.start();
+        redAnim.start();
     }
 
 }
