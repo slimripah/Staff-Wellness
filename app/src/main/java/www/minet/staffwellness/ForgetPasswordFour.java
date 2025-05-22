19448a495899853e0bb94ad3e5930333101c6651
slimripah <slimripah@gmail.com>
2025-05-22 10:16:51 +0300

    forget password design

--- app/src/main/java/www/minet/staffwellness/ForgetPasswordFour.java
+++ app/src/main/java/www/minet/staffwellness/ForgetPasswordFour.java
@@ -1,6 +1,11 @@
 package www.minet.staffwellness;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ForgetPasswordFour extends AppCompatActivity {
+
+    Button next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,20 @@ public class ForgetPasswordFour extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        next = findViewById(R.id.loginDos);
+
+        next.setOnClickListener(v -> {
+
+            Intent intent = new Intent(ForgetPasswordFour.this, Login.class);
+
+            //add transition
+            Pair[] pairs = new Pair[1];
+            pairs[0] = new Pair<View, String>(findViewById(R.id.loginDos), "transition_login");
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(ForgetPasswordFour.this, pairs);
+            startActivity(intent, options.toBundle());
+
         });
 
     }
--- app/src/main/java/www/minet/staffwellness/ForgetPasswordThree.java
+++ app/src/main/java/www/minet/staffwellness/ForgetPasswordThree.java
@@ -1,6 +1,11 @@
 package www.minet.staffwellness;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ForgetPasswordThree extends AppCompatActivity {
+
+    Button next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,20 @@ public class ForgetPasswordThree extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        next = findViewById(R.id.btn_next);
+
+        next.setOnClickListener(v -> {
+
+            Intent intent = new Intent(ForgetPasswordThree.this, ForgetPasswordFour.class);
+
+            //add transition
+            Pair[] pairs = new Pair[1];
+            pairs[0] = new Pair<View, String>(findViewById(R.id.btn_next), "transition_tres");
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(ForgetPasswordThree.this, pairs);
+            startActivity(intent, options.toBundle());
+
         });
 
     }
